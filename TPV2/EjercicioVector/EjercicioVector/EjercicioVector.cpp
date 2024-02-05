58d2632e6796973f38d84bd8e43c4285c32c9678
Paigro <pabigl04@ucm.es>
2024-02-05 18:05:54 +0100

    Ejercicio vector.

--- TPV2/EjercicioVector/EjercicioVector/EjercicioVector.cpp
+++ TPV2/EjercicioVector/EjercicioVector/EjercicioVector.cpp
@@ -29,12 +29,12 @@ void reset_stats() {
 void print_stats() {
 	std::cout << "--------------" << std::endl;
 	std::cout << "new int: " << num_of_new_int << std::endl; // objeto creado(new)
-	std::cout << "del int: " << num_of_delete << std::endl; // objeto eliminado
-	std::cout << "const_d: " << num_of_const_d << std::endl; // destructora del objeto creado
-	std::cout << "const_c: " << num_of_const_c << std::endl; // objeto copiado
-	std::cout << "const_m: " << num_of_const_m << std::endl; // obejto movido
-	std::cout << "assig_c: " << num_of_assig_c << std::endl; // objeto asignado por constructora normal
-	std::cout << "assig_m: " << num_of_assig_m << std::endl; // objeto asignado por movimiento
+	std::cout << "delete int: " << num_of_delete << std::endl; // objeto eliminado
+	std::cout << "constructor_default: " << num_of_const_d << std::endl; // constructora del objeto creado
+	std::cout << "construtora_copia: " << num_of_const_c << std::endl; // objeto copiado
+	std::cout << "constructora_move: " << num_of_const_m << std::endl; // obejto movido
+	std::cout << "assig_copia: " << num_of_assig_c << std::endl; // objeto asignado por copia
+	std::cout << "assig_move: " << num_of_assig_m << std::endl; // objeto asignado por movimiento
 	std::cout << "--------------" << std::endl;
 
 }
@@ -92,13 +92,12 @@ public:
 		p = o.p;
 		o.p = nullptr;
 		num_of_assig_m++;
-		std::cout << "Move. assign. *p=" << *p << ", moved " << &o << " to "
-			<< this << std::endl;
+		std::cout << "Move. assign. *p=" << *p << ", moved " << &o << " to " << this << std::endl;
 		return *this;
 	}
 
 	~A() {
-		std::cout << "Dtor." << std::endl;
+		std::cout << "Destructor." << std::endl;
 		num_of_dest++;
 		if (p != nullptr) {
 			num_of_delete++;
@@ -120,25 +119,69 @@ void print_vector(const std::vector<T>& v) {
 }
 
 void test0() {
-	/*std::vector<A> v;
-	v.push_back(A(1));*/
+	std::vector<A> v;
+	v.push_back(A(1));
 
-	/*std::vector<A> v;
+	std::cout << "------------BUENOS DIAS------------" << std::endl;
+}
+
+void test1() {
+	std::vector<A> v;
 	A x(1);
-	v.push_back(x);*/
+	v.push_back(x);
 
+	std::cout << "------------BUENOS DIAS------------" << std::endl;
+}
+
+void test2() {
 	std::vector<A> v;
 	A x(1);
 	v.push_back(std::move(x));
 
-	/*std::vector<A> v;
-	v.emplace_back(1);*/
+	std::cout << "------------BUENOS DIAS------------" << std::endl;
 }
 
+void test3() {
+	std::vector<A> v;
+	v.emplace_back(1);
+
+	std::cout << "------------BUENOS DIAS------------" << std::endl;
+}
+
+void test4() {
+	std::vector<A> v;
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << "# adding " << i << " using emplace_back" << std::endl;
+		if (v.capacity() == v.size()) {
+			std::cout << "# The vector is about to be resized!" << std::endl;
+		}
+		v.emplace_back(i);
+	}
+}
+
+void test5() {
+	std::vector<A> v;
+	v.reserve(20);
+	for (int i = 0; i < 10; i++) {
+		std::cout << "# adding " << i << " using emplace_back" << std::endl;
+		if (v.capacity() == v.size()) {
+			std::cout << "# The vector is about to be resized!" << std::endl;
+		}
+		v.emplace_back(i);
+	}
+}
 
 int main(int, char**) {
 	reset_stats();
+
 	test0();
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
+
 	print_stats();
 
 	return 0;
