9719c185074542aea38449b7f2c3573a5adde41b
Paigro <paigro9@gmail.com>
2024-04-07 13:30:12 +0200

    Filtros otra vez u ok?

--- TPV2/Practica2/TPV2/TPV2/src/systems/ImmunitySystem.cpp
+++ TPV2/Practica2/TPV2/TPV2/src/systems/ImmunitySystem.cpp
@@ -1,1 +1,19 @@
 #include "ImmunitySystem.h"
+
+
+ImmunitySystem::ImmunitySystem() {
+
+}
+
+void ImmunitySystem::initSystem() {
+
+}
+
+void ImmunitySystem::update() 
+{
+
+}
+void ImmunitySystem::recieve(const Message& men) 
+{
+
+}
--- TPV2/Practica2/TPV2/TPV2/src/systems/ImmunitySystem.h
+++ TPV2/Practica2/TPV2/TPV2/src/systems/ImmunitySystem.h
@@ -1,5 +1,28 @@
 #pragma once
-class ImmunitySystem
+
+#include "../ecs/System.h"
+
+const uint32_t TIME_INMUNE = 10000; // Tiempo maximo que puede estar el PacMan en estado inmune (10s).
+
+class ImmunitySystem : public ecs::System
 {
+public:
+	__SYSID_DECL__(ecs::sys::INMUNITY);
+	// Constructora.
+	ImmunitySystem();
+	// Destructora.
+	virtual ~ImmunitySystem() {};
+
+	// Init del sistema.
+	void initSystem() override;
+	// Update del sistema.
+	void update() override;
+	// Para recibir mensajes.
+	void recieve(const Message& m) override;
+private:
+
+	bool isInmune = false;
+	uint32_t time;
+
 };
 
