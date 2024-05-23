88127388c2a496ccea29aedb29e89aefa8c0a446
AndresGarcia :) <andrga37@ucm.es>
2024-05-23 16:49:52 +0200

    hace un año cuando yo era joven no me fijaba en tus pantalones
    no queria ver la tele contigo no te gustaban todos mis amigos

---
+++ IG/Nueva carpeta/ProyectosIG_x64_VS2022/IG1App/Material.cpp
@@ -0,0 +1,36 @@
+#include "Material.h"
+#include <glm/gtc/type_ptr.hpp>
+
+//------Ejercicio74:
+void Material::upload()
+{
+	glColorMaterial(face, GL_SPECULAR);
+	glColorMaterial(face, GL_SHININESS);
+	glColorMaterial(face, GL_AMBIENT);
+	glColorMaterial(face, GL_DIFFUSE);
+	// Ajusta caracteristicas del material
+	glMaterialfv(face, GL_AMBIENT, value_ptr(ambient));
+	glMaterialfv(face, GL_DIFFUSE, value_ptr(diffuse));
+	glMaterialfv(face, GL_SPECULAR, value_ptr(specular));
+	glMaterialf(face, GL_SHININESS, expF);
+	// Shadea el modelo
+	glShadeModel(sh);
+}
+
+void Material::setCopper()
+{
+	ambient = { 0.19125, 0.0735, 0.0225, 1 };
+	diffuse = { 0.7038, 0.27048, 0.0828, 1 };
+	specular = { 0.256777, 0.137622, 0.086014, 1 };
+	expF = 12.8;
+}
+
+void Material::reset()
+{
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+
+	glMaterialfv(face, GL_AMBIENT, value_ptr(glm::fvec4(0.2, 0.2, 0.2, 1)));
+	glMaterialfv(face, GL_DIFFUSE, value_ptr(glm::fvec4(0.8, 0.8, 0.8, 1)));
+	glMaterialfv(face, GL_SPECULAR, value_ptr(glm::fvec4(0.0, 0.0, 0.0, 1)));
+}
---
+++ IG/Nueva carpeta/ProyectosIG_x64_VS2022/IG1App/Material.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include "Entity.h"
+
+//------Ejercicio74:
+class Material {
+protected:
+	// Coeficientes de reflexión
+	glm::fvec4 ambient = { 0.2, 0.2, 0.2, 1.0 };
+	glm::fvec4 diffuse = { 0.8, 0.8, 0.8, 1.0 };
+	glm::fvec4 specular = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat expF = 0;
+	// Exponente para la reflexión especular
+	GLuint face = GL_FRONT_AND_BACK;
+	GLuint sh = GL_SMOOTH; // Tipo de matizado
+public:
+	Material() {};
+	virtual ~Material() {};
+	virtual void upload();
+	void setCopper();
+	void reset();
+};
